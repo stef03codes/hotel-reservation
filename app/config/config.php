ea7822ae1937d553f30ff276f03cd31425b696a2
stef03codes <129624887+stef03codes@users.noreply.github.com>
2024-09-16 20:52:15 +0200

    Make the file structure

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+// <-- Connect to database -->
+$db_server = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name = "shop";
+
+$conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+
+if(!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
